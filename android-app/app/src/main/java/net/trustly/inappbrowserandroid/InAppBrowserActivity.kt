0e1732e04f11400c3eabf38e42f45986e527fe6d
Andre Guedes <andre.guedes@trustly.com>
2025-09-01 10:22:15 -0300

    Opening url on custom tabs

--- android-app/app/src/main/java/net/trustly/inappbrowserandroid/InAppBrowserActivity.kt
+++ android-app/app/src/main/java/net/trustly/inappbrowserandroid/InAppBrowserActivity.kt
@@ -3,7 +3,6 @@ package net.trustly.inappbrowserandroid
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.net.toUri
@@ -14,7 +13,32 @@ class InAppBrowserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val openLightboxDirectly = intent.getBooleanExtra(OPEN_LIGHTBOX_DIRECTLY, false)
-        Toast.makeText(this, "Open Lightbox: $openLightboxDirectly", Toast.LENGTH_SHORT).show()
+        val url =
+            "https://sandbox.paywithmybank.com/frontend/mobile/establish?widget=$openLightboxDirectly&token=\\(establishBase64)"
+        launchUrl(this, url)
+    }
+
+    private fun getEstablishDataValues(context: Context): MutableMap<String, String> {
+        val establishDataValues: MutableMap<String, String> = HashMap()
+        establishDataValues["accessId"] = "<ACCESS_ID>"
+        establishDataValues["amount"] = "0.00"
+        establishDataValues["merchantId"] = "<MERCHANT_ID>"
+        establishDataValues["currency"] = "USD"
+        establishDataValues["merchantReference"] = "<MERCHANT_REFERENCE>"
+        establishDataValues["paymentType"] = "Retrieval"
+        establishDataValues["env"] = "<[int, sandbox, local]>"
+        establishDataValues["description"] = "Android InAppBrowser"
+        establishDataValues["localUrl"] = "<YOUR LOCAL URL WHEN `ENV` PROPERTY IS `LOCAL` (ex: https://192.168.0.30)>"
+        establishDataValues["customer.name"] = "John Smith Android"
+        establishDataValues["customer.address.address1"] = "2000 Broadway Street"
+        establishDataValues["customer.address.city"] = "Redwood City"
+        establishDataValues["customer.address.state"] = "CA"
+        establishDataValues["customer.address.zip"] = "94063"
+        establishDataValues["customer.address.country"] = "US"
+        establishDataValues["customer.phone"] = "2145553434"
+        establishDataValues["customer.email"] = "jsmith@email.com"
+        establishDataValues["metadata.urlScheme"] = context.getString(R.string.url_scheme) + "://"
+        return establishDataValues
     }
 
     private fun launchUrl(context: Context, url: String) {
