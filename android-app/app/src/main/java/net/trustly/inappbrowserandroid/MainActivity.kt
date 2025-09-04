cf27cf47a326e8a90e4e67dc3bf30373900ac303
Andre Guedes <andre.guedes@trustly.com>
2025-09-04 14:40:22 -0300

    Changing main screen

--- android-app/app/src/main/java/net/trustly/inappbrowserandroid/MainActivity.kt
+++ android-app/app/src/main/java/net/trustly/inappbrowserandroid/MainActivity.kt
@@ -1,11 +1,8 @@
 package net.trustly.inappbrowserandroid
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import net.trustly.inappbrowserandroid.webchromeclient.WebChromeClientActivity
-import net.trustly.inappbrowserandroid.webviewclient.WebViewClientActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,14 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webViewClientButton = findViewById<Button>(R.id.webViewClientButton)
+        val webViewClientButton = findViewById<Button>(R.id.widgetButton)
         webViewClientButton.setOnClickListener {
-            startActivity(Intent(this, WebViewClientActivity::class.java))
+            //TODO Open widget
         }
 
-        val webViewChromeButton = findViewById<Button>(R.id.webViewChromeButton)
+        val webViewChromeButton = findViewById<Button>(R.id.lightboxButton)
         webViewChromeButton.setOnClickListener {
-            startActivity(Intent(this, WebChromeClientActivity::class.java))
+            //TODO Open lightbox
         }
     }
 
