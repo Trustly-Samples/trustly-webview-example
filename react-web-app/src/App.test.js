e6cf66191a1b9fe17d6172eebafa6d3db79e3e0e
Pedro Abdenor <pedro.abdenor@trustly.com>
2025-09-11 09:19:10 -0300

    Add instructions for enviroments

--- react-web-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Trustly Pay Card', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Pay with Trustly/i);
-  expect(linkElement).toBeInTheDocument();
-});
