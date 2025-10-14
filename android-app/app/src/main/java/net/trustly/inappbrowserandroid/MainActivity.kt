a1aa18e68eb1ab6ce72f165e8436f3b1257d4f86
Andre Guedes <andre.guedes@trustly.com>
2025-10-14 10:33:21 -0300

    Adding dialog for success or failure message

--- android-app/app/src/main/java/net/trustly/inappbrowserandroid/MainActivity.kt
+++ android-app/app/src/main/java/net/trustly/inappbrowserandroid/MainActivity.kt
@@ -2,7 +2,9 @@ package net.trustly.inappbrowserandroid
 
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +21,36 @@ class MainActivity : AppCompatActivity() {
         lightboxButton.setOnClickListener {
             startActivity(InAppBrowserActivity.startIntent(this, true))
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (intent.hasExtra(ESTABLISH_DATA)) {
+            val establishDataValues =
+                intent.getSerializableExtra(ESTABLISH_DATA) as Map<String, String>
+            if (establishDataValues.contains(STATUS_PARAM)) {
+                val status = establishDataValues[STATUS_PARAM].equals(STATUS_SUCCESS)
+                if (status) showDialogResult(true)
+                else showDialogResult(false)
+            }
+        }
+    }
+
+    private fun showDialogResult(result: Boolean) {
+        val message = if (result) "Authorization successful!" else "Authorization failed!"
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setMessage(message).setTitle("Authorization")
+            .setNeutralButton("OK") { dialog, _ -> dialog.dismiss() }
+        builder.create().show()
+    }
+
+    companion object {
+
+        const val ESTABLISH_DATA = "establishData"
+        const val STATUS_PARAM = "status"
+        const val STATUS_SUCCESS = "2"
+
     }
 
 }
